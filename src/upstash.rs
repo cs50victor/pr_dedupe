506c4eb12ca2b03ffdbac36ac1a9124bc02a4df6
victor <52110451+cs50victor@users.noreply.github.com>
2024-02-10 18:25:11 -0500

    fix: got embedding & upstash query to work (#8)

--- src/upstash.rs
+++ src/upstash.rs
@@ -1,12 +1,52 @@
 use std::env;
 
-use anyhow::Result;
-use reqwest::{header, Client, Response, Url};
+use anyhow::{bail, Result};
+
+use reqwest::{header, Client, Url};
+use serde::{Deserialize, Serialize};
 use serde_json::json;
+
+use crate::{SimilarPRs, SimilarPRsInner};
 
 pub struct Upstash {
     client: Client,
     url_endpoint: Url,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Data {
+    id: String,
+    score: f32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct QueryResult {
+    result: Vec<Data>,
+}
+
+impl From<QueryResult> for SimilarPRs {
+    fn from(val: QueryResult) -> Self {
+        let add_pr_prefix = |pr_number: &str| {
+            format!(
+                "https://github.com/{}/pull/{pr_number}",
+                env::var("REPO_NAME").unwrap(),
+            )
+        };
+
+        SimilarPRs {
+            data: val
+                .result
+                .iter()
+                .map(|d| {
+                    let pr_number = d.id.split(':').next_back().unwrap();
+                    SimilarPRsInner {
+                        pr_url: add_pr_prefix(pr_number),
+                        percentage: d.score * 100.0,
+                    }
+                })
+                .collect::<Vec<_>>(),
+        }
+    }
 }
 
 impl Upstash {
@@ -31,39 +71,50 @@ impl Upstash {
         })
     }
 
-    pub async fn save_embedding(&self, embedding: &Vec<f32>) -> Result<Response> {
+    pub async fn save_embedding(&self, embedding: &Vec<f32>) -> Result<()> {
         let (repo_name, pr_number) = (env::var("REPO_NAME")?, env::var("PR_NUMBER")?);
+
         let data = json!({
             "id": uuid(&repo_name,&pr_number),
             "vector": embedding,
-        });
+        })
+        .to_string();
 
-        let resp = self
-            .client
-            .post(format!("{}/upsert", self.url_endpoint))
-            .body(data.to_string())
-            .send()
-            .await?;
+        let uri = self.url_endpoint.join("upsert")?;
 
-        Ok(resp)
+        let resp = self.client.post(uri).body(data).send().await?;
+
+        if resp.status().as_u16() != 200 {
+            bail!(
+                "Couldn't save embedding | Reason {}",
+                resp.text().await.unwrap()
+            );
+        }
+
+        Ok(())
     }
 
-    pub async fn query(&self, embedding: &Vec<f32>, top_k: u8) -> Result<Response> {
+    pub async fn query(&self, embedding: &Vec<f32>, top_k: u8) -> Result<SimilarPRs> {
         let data = json!({
             "topK": top_k,
             "vector": &embedding,
-            "includeVectors": false,
-            "includeMetadata": false
-        });
+        })
+        .to_string();
 
-        let resp = self
-            .client
-            .post(format!("{}/query", self.url_endpoint))
-            .body(data.to_string())
-            .send()
-            .await?;
+        let uri = self.url_endpoint.join("query")?;
 
-        Ok(resp)
+        let resp = self.client.post(uri).body(data).send().await?;
+
+        if resp.status().as_u16() != 200 {
+            bail!(
+                "Couldn't query db for similar PRs | Reason {}",
+                resp.text().await.unwrap()
+            );
+        }
+
+        let results = serde_json::from_str::<QueryResult>(&resp.text().await.unwrap())?;
+
+        Ok(results.into())
     }
 }
 
