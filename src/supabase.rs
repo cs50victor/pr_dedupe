84d114f0ec0a746846571e7776827651e437939e
victor <52110451+cs50victor@users.noreply.github.com>
2024-02-12 05:58:40 -0500

    feat: only return results for current repo (#17)

--- src/supabase.rs
+++ src/supabase.rs
@@ -1,26 +1,34 @@
+// add supabase later
+use std::env;
+
+use anyhow::bail;
 use postgrest::Postgrest;
 use serde_json::json;
 
-// add later
+use anyhow::Result;
 
 struct SB {
     client: Postgrest,
 }
 
 impl SB {
-    pub fn new<T>(supabase_url: T, supabase_service_role_key: impl AsRef<str>) -> Self
-    where
-        T: Into<String>,
-    {
-        Self {
-            client: Postgrest::new(supabase_url).insert_header("apikey", supabase_service_role_key), // TODO: add later
-                                                                                                     // {
-                                                                                                     //     auth: {
-                                                                                                     //       persistSession: false,
-                                                                                                     //       autoRefreshToken: false,
-                                                                                                     //     },
-                                                                                                     //   }
+    pub fn new() -> Result<Self> {
+        let (supabase_url, supabase_service_role_key) = (
+            env::var("SUPABASE_URL"),
+            env::var("SUPABASE_SERVICE_ROLE_KEY"),
+        );
+
+        if supabase_url.is_err() || supabase_service_role_key.is_err() {
+            bail!("both SUPABASE_URL and SUPABASE_SERVICE_ROLE_KEY env variables need to be set to use supabase's vector database");
         }
+
+        let (supabase_url, supabase_service_role_key) =
+            (supabase_url.unwrap(), supabase_service_role_key.unwrap());
+
+        Ok(Self {
+            // TODO: add later
+            client: Postgrest::new(supabase_url).insert_header("apikey", supabase_service_role_key),
+        })
     }
 
     pub async fn save_embedding(&self, embedding: Vec<f32>) {
--- src/utils.rs
+++ src/utils.rs
@@ -1,43 +1,27 @@
 use std::{env, process::exit};
 
-use anyhow::{bail, Result};
 use log::error;
 
-pub fn get_upstash_envs() -> Result<(String, String)> {
-    let (upstash_vector_rest_url, upstash_vector_rest_token) = (
-        env::var("UPSTASH_VECTOR_REST_URL"),
-        env::var("UPSTASH_VECTOR_REST_TOKEN"),
-    );
+use anyhow::Result;
 
-    if upstash_vector_rest_url.is_err() || upstash_vector_rest_token.is_err() {
-        bail!("both UPSTASH_VECTOR_REST_URL and UPSTASH_VECTOR_REST_TOKEN env variables need to be set to use supabase's vector database");
-    }
+use crate::SimilarPRs;
 
-    Ok((
-        upstash_vector_rest_url.unwrap(),
-        upstash_vector_rest_token.unwrap(),
-    ))
-}
-
-pub fn get_supabase_envs() -> Result<(String, String)> {
-    let (supabase_url, supabase_service_role_key) = (
-        env::var("SUPABASE_URL"),
-        env::var("SUPABASE_SERVICE_ROLE_KEY"),
-    );
-
-    if supabase_url.is_err() || supabase_service_role_key.is_err() {
-        bail!("both SUPABASE_URL and SUPABASE_SERVICE_ROLE_KEY env variables need to be set to use supabase's vector database");
-    }
-
-    Ok((supabase_url.unwrap(), supabase_service_role_key.unwrap()))
+pub trait VectorDB {
+    async fn save_embedding(&self, embedding: &[f32]) -> Result<()>;
+    async fn remove_pr(&self) -> Result<()>;
+    async fn query(&self, embedding: &[f32], top_k: u8, min_similarity: u8) -> Result<SimilarPRs>;
 }
 
 pub fn uuid(repo_name: &str, pr_number: &str) -> String {
-    format!("[{repo_name}]:{pr_number}")
+    format!("{repo_name}:{pr_number}")
 }
 
 pub fn uuid_to_pr_number(uuid: &str) -> &str {
     uuid.split(':').next_back().unwrap()
+}
+
+pub fn uuid_to_repo_name(uuid: &str) -> &str {
+    uuid.split(':').next().unwrap()
 }
 
 pub fn log_err_and_exit(msg: impl AsRef<str>) -> ! {
@@ -58,12 +42,22 @@ mod tests {
     use super::*;
 
     #[test]
-    fn uuid_encode_and_decode() {
+    fn uuid_encode_and_decode_pr_number() {
         let repo_name = "cs50victor/pr_dedupe";
         let pr_number = "2";
 
         let uuid = uuid(repo_name, pr_number);
 
         assert_eq!(uuid_to_pr_number(&uuid), pr_number);
+    }
+
+    #[test]
+    fn uuid_encode_and_decode_repo_name() {
+        let repo_name = "cs50victor/pr_dedupe";
+        let pr_number = "2";
+
+        let uuid = uuid(repo_name, pr_number);
+
+        assert_eq!(uuid_to_repo_name(&uuid), repo_name);
     }
 }
